8e7c64d34a8c2715ee91259a89f4f859248c698f
parthkumar9 <parthkumar9@outlook.com>
2024-03-18 23:24:05 -0600

    Initial Commit

---
+++ client/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    
+
+    <Button size="lg" variant="custom" >Hello Auth!</Button>
+    
+    </>
+  );
+}
