c98cf0832d628e146357331e0b2ff080798e4ae2
parthkumar9 <parthkumar9@outlook.com>
2024-03-20 23:26:21 -0600

    HomePage & Routing

---
+++ client/components/auth/login-button.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+interface LoginButtonProps {
+  children: React.ReactNode;
+  mode?: "modal" | "redirect";
+  asChild?: boolean;
+}
+
+export const LoginButton = ({
+  children,
+  mode = "redirect",
+  asChild,
+}: LoginButtonProps) => {
+    const onClick = () => {
+        console.log("Login Button")
+    }
+  return <span onClick={onClick} className="cursor-pointer">{children}</span>;
+};
